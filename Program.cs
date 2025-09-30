bb096a3e22128b68942b4fddf1bbbf26326836b6
sipihri <ihrisip@gmail.com>
2025-09-30 18:19:47 +0500

    Add ProjectsView.cs for selecting projects

--- Program.cs
+++ Program.cs
@@ -3,11 +3,37 @@ using Tusky;
 using Tusky.Data;
 using Tusky.Services;
 
-var filePath = "tasks.json";
+var filePath = "";
 
 if (args.Length > 0)
 {
     filePath = args[0];
+}
+
+if (string.IsNullOrEmpty(filePath))
+{
+    var projectsView = new ProjectsView("./");
+    AnsiConsole.AlternateScreen(() =>
+    {
+        try
+        {
+            projectsView.Run();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            Console.ReadKey();
+        }
+    });
+
+    if (projectsView.ExitRequested)
+    {
+        return;
+    }
+    else
+    {
+        filePath = projectsView.SelectedProject;
+    }
 }
 
 ITaskRepository repository = new FileSystemTaskRepository(filePath);
