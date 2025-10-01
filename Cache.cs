a66b07c0b7f081d740e174d24eac4419a2b51df6
sipihri <ihrisip@gmail.com>
2025-10-01 08:10:07 +0500

    Add Cache.cs

---
+++ Cache.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.Json;
+
+namespace Tusky;
+
+public class Cache
+{
+    public string LastProjectPath { get; set; } = string.Empty;
+    
+    public static Cache Load(string path)
+    {
+        if (File.Exists(path) == false)
+        {
+            var cache = new Cache();
+            cache.Save(path);
+            return cache;
+        }
+        try
+        {
+            string json = File.ReadAllText(path);
+            var cache = JsonSerializer.Deserialize<Cache>(json);
+
+            return cache ?? new Cache();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return new Cache();
+        }
+    }
+
+    public void Save(string path)
+    {
+        try
+        {
+            string json = JsonSerializer.Serialize(this);
+            File.WriteAllText(path, json);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
