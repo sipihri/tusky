4a1013f459722bf40c929ab0907ccbd3d5739cd1
sipihri <ihrisip@gmail.com>
2025-10-03 19:31:34 +0500

    Moved AppState to its own file

--- App.cs
+++ App.cs
@@ -1,33 +1,7 @@
 using Tusky.Services;
-using Tusky.Utils;
 using Tusky.Views;
 
 namespace Tusky;
-
-public class AppState
-{
-    public enum TextInputActionType
-    {
-        Add,
-        Edit
-    }
-    
-    public bool ShowTextInput;
-    public readonly TextInput TextInput = new();
-    public TextInputActionType CurrentTextInputActionType;
-
-    public TaskService.FilterMode FilterMode;
-
-    public TaskService.SortMode SortMode;
-
-    public int SelectedIndex;
-    public bool ShouldQuit;
-    
-    /// <summary>
-    /// Returns true if the text input was committed and the requested action was to add task
-    /// </summary>
-    public bool IsAddingTask => ShowTextInput && CurrentTextInputActionType == TextInputActionType.Add;
-}
 
 public class App
 {
---
+++ AppState.cs
@@ -0,0 +1,29 @@
+﻿using Tusky.Services;
+using Tusky.Utils;
+
+namespace Tusky;
+
+public class AppState
+{
+    public enum TextInputActionType
+    {
+        Add,
+        Edit
+    }
+    
+    public bool ShowTextInput;
+    public readonly TextInput TextInput = new();
+    public TextInputActionType CurrentTextInputActionType;
+
+    public TaskService.FilterMode FilterMode;
+
+    public TaskService.SortMode SortMode;
+
+    public int SelectedIndex;
+    public bool ShouldQuit;
+    
+    /// <summary>
+    /// Returns true if the text input was committed and the requested action was to add task
+    /// </summary>
+    public bool IsAddingTask => ShowTextInput && CurrentTextInputActionType == TextInputActionType.Add;
+}
